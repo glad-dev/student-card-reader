bbe0300b0a3900620ee8c7daeed78fc3211cef0c
Gabriel Loyola Daiqui <git@loyola.dev>
2023-11-20 18:48:56 +0100

    Result is hidden after 4 seconds
    However, there is no animation, which makes it a bit jarring

--- frontend/src/Result.svelte
+++ frontend/src/Result.svelte
@@ -1,22 +1,37 @@
 <script lang="ts">
+	import {onMount} from "svelte";
+
 	export let error: string
+
+	let show: boolean = true
+
+	onMount(() => {
+		const timeout = setTimeout(() => {
+			show = false
+		}, 4000)
+
+		return () => clearTimeout(timeout)
+	})
 </script>
 
-{#if error == null}
-	<h3 class="denied">An internal error occurred: Error is null</h3>
-{:else}
-	{#if error.length === 0}
-		<h3 class="allowed">The student is allowed to get an item</h3>
-	{:else}
-		<div class="denied">
-			<h3>The student is not allowed to get an item</h3>
-			<p>{error}</p>
-		</div>
-	{/if}
+{#if show}
+	<div>
+		{#if error == null}
+			<h3 class="denied">An internal error occurred: Error is null</h3>
+		{:else}
+			{#if error.length === 0}
+				<h3 class="allowed">The student is allowed to get an item</h3>
+			{:else}
+				<div class="denied">
+					<h3>The student is not allowed to get an item</h3>
+					<p>{error}</p>
+				</div>
+			{/if}
+		{/if}
 
-	<br>
+		<br>
+	</div>
 {/if}
-
 
 <style>
 	.allowed {
